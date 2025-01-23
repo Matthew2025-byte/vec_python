a6bdf6963a9ffd2aa72691bc87d20cebbe9d99fd
Matthew2025-byte <mdstapleton2020@outlook.com>
2025-01-23 13:04:16 -0800

    Initial commit

--- vector2.py
+++ vector2.py
@@ -7,10 +7,28 @@ class vec2:
         self.x = x
         self.y = y
 
+
+
     def sqrmagnitude(self):
         return self.x**2 + self.y**2
     def magnitude(self):
         return sqrt(self.x**2 + self.y**2)
 
+
     def normalized(self):
         self / self.magnitude()
+
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return vec2(self.x * other, self.y * other)
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return vec2(self.x / other, self.y / other)
+
+    def __add__(self, other):
+        if isinstance(other, vec2):
+            return vec2(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        if isinstance(other, vec2):
+            return vec2(self.x - other.x, self.y - other.y)
